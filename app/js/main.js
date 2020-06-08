31abb90ef5a4082c16c25b575039a7a99aebdcd9
milan.zivanov <milan.zivanov@gmail.com>
2020-06-09 00:42:13 +0200

    progress added scroll to section 07

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,20 @@
 // https://github.com/mattboldt/typed.js/
 var typed = new Typed('.element', {
-    strings: ["Veleprodaja"],
+    strings: ["Proizvodnja"],
     typeSpeed: 100
+});
+
+// scroll to section
+$('a[href^="#"]').bind('click', function(e) {
+    e.preventDefault(); 
+
+    var target = $(this).attr("href");
+
+    $('html, body').stop().animate({
+        scrollTop: $(target).offset().top
+    }, 700, function() {
+        location.hash = target; 
+    });
+
+    return false;
 });
